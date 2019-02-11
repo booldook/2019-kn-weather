9e1ee073a2000d0ead37311af3046736f55be126
Daeseang <eotmdzla@gmail.com>
2019-02-11 17:09:05 +0900

    Daily 완성

--- public/js/main.js
+++ public/js/main.js
@@ -54,13 +54,17 @@ $(".nav").eq(0).trigger("click");
 
 $("#city").change(function(){
   option.id = $(this).val();
-  $.ajax({
+  var sendData = {
     type: "get",
-    url: apiURL + files[0],
-    data: option,
     dataType: "json",
-    success: dailyInit
-  });
+    data: option,
+  }
+  sendData.url = apiURL + files[0];
+  sendData.success = dailyInit;
+  $.ajax(sendData);
+  sendData.url = apiURL + files[1];
+  sendData.success = weeklyInit;
+  $.ajax(sendData);
 });
 function dailyInit(data) {
   console.log(data);
@@ -68,14 +72,18 @@ function dailyInit(data) {
   var $daily = $("#daily");
   var src = "../img/icon/"+data.weather[0].icon+".png";
   var temp = data.main.temp+" ℃";
-  var temp2 = data.main.temp_max+" ℃ / "+data.main.temp_min+" ℃ / "
+  var temp2 = data.main.temp_max+" ℃ / "+data.main.temp_min+" ℃"
   var html = '';
   html += '<ul>';
   html += '<li class="icon"><img src="'+src+'" class="img"></li>';
   html += '<li class="city_name">'+$("#city > option:selected").text()+'</li>';
   html += '<li class="w3-center"><button class="w3-button w3-indigo" onclick="cityInit();">도시선택</button></li>';
-  html += '<li class="temp">현재평균온도: '+temp+'</li>';
-  html += '<li class="temp2">최고/최저온도: '+temp2+'</li>';
+  html += '<li class="temp">현재평균온도: <b class="w3-text-indigo">'+temp+'</b></li>';
+  html += '<li class="temp2">최고/최저온도: <b class="w3-text-indigo">'+temp2+'</b></li>';
   html += '</ul>';
   $daily.html(html);
+}
+
+function weeklyInit(data) {
+  console.log(data);
 }
